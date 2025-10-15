7f2a14e6aed9394ea6bee9de136893239c95431a
Charlie-Root <wieger.bontekoe@gmail.com>
2025-10-15 11:04:13 +0200

    fix

--- netbox_powerdns_sync/__init__.py
+++ netbox_powerdns_sync/__init__.py
@@ -1,6 +1,8 @@
 from netbox.plugins import PluginConfig
 from .version import __version__
 from django.dispatch import receiver
+from django.db.models.signals import post_migrate
+
 
 class NetBoxPowerdnsSyncConfig(PluginConfig):
     name = "netbox_powerdns_sync"
